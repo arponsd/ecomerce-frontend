00fe63cbdaf066f5971d97f54d3680c1c0020f9f
Arpon Sutradhar <106023801+arponsd@users.noreply.github.com>
2024-05-09 01:51:38 +0600

    Install primeNG done

--- src/app/layouts/component/default-header/default-header.component.ts
+++ src/app/layouts/component/default-header/default-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './default-header.component.html'
 })
 export class DefaultHeaderComponent {
