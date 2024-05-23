0d3ac2e1d0a9514db3291658fc5af0a610b73513
Arpon Sutradhar <106023801+arponsd@users.noreply.github.com>
2024-05-24 00:03:57 +0600

    worked on tailwind config

--- src/app/layouts/component/default-header/default-header.component.ts
+++ src/app/layouts/component/default-header/default-header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, InputTextModule, FloatLabelModule],
   templateUrl: './default-header.component.html'
 })
 export class DefaultHeaderComponent {
