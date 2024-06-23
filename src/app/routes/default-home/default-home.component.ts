522ee881e90c7c6fed6df843b6b64b0d37450551
Arpon Sutradhar <106023801+arponsd@users.noreply.github.com>
2024-06-23 23:40:08 +0600

    worked on design

--- src/app/routes/default-home/default-home.component.ts
+++ src/app/routes/default-home/default-home.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-default-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './default-home.component.html'
 })
-export class DefaultHomeComponent {
+export class DefaultHomeComponent implements OnInit  {
+  selectedButton = signal<number>(1);
+
+  constructor() {}
+  ngOnInit(): void {
+    this.onClickOption(this.selectedButton());
+  }
+
+  onClickOption(n: number) {
+    this.selectedButton.set(n);
+    switch(this.selectedButton()) {
+      case 1: {
+        console.log(`this is ${this.selectedButton()}`);
+        break;
+      }
+      case 2: {
+        console.log(`this is ${this.selectedButton()}`);
+        break;
+      }
+      case 3: {
+        console.log(`this is ${this.selectedButton()}`);
+        break;
+      }
+    }
+  }
 
 }
